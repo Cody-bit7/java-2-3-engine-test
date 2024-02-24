7198398ae8ea70ffc87012092c8a284397e10402
Justin Kruskie <kruskiej@baisd.net>
2024-02-24 16:23:12 +0000

    Create assignment

---
+++ EngineTest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EngineTest 
+{
+    public static void main(String[] args) 
+    {
+        // Create your variables.    
+        
+        // Create a scanner object for the keyboard.
+
+        // Get the fuel per second used.
+
+        // Get the length of the burn in seconds.
+
+        // Calculate the amount of fuel required.
+
+        // Display the fuel required.
+
+        // Exit the program.
+
+    }
+}
