ccb2a2651c59c3f63608b2cde28094558caca771
Cody-bit7 <cjmckeon9@gmail.com>
2025-09-10 09:59:11 -0400

    fixed 2-3

--- EngineTest.java
+++ EngineTest.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class EngineTest {
 
+    private static Scanner input;
+
     public static void main(String[] args) {
         // Create your variables.    
         double fuelrequired;
@@ -11,17 +13,19 @@ public class EngineTest {
         Scanner keyboard = new Scanner(System.in);
         // Get the fuel per second used.
         System.out.println("how many kilograms of fuel does the genine use per second?");
+        
         // Get the length of the burn in seconds.
         fuelpersecond = keyboard.nextDouble();
+        System.out.println(fuelpersecond + 20);
         System.out.println("for how many seconds will the engine be on?");
         burntime = keyboard.nextDouble();
+        System.out.println(burntime + 40);
         // Calculate the amount of fuel required.
         fuelrequired = fuelpersecond * burntime;
+        System.out.println(fuelrequired = burntime *fuelpersecond);
         // Display the fuel required.
-         System.out.println("how much fuel is required?");
-        fuelrequired = keyboard.nextDouble();
         // Exit the program.
-
+         System.exit(0);
     }
     
 }
